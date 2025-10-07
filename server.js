fcc09ad70c94884f2928d571920ed43d3dc67ad2
Willian Cabezas <cabezaswiiliam@unicomfacauca.edu.co>
2025-10-06 19:35:24 -0500

    Fix: escuchar en 0.0.0.0 para AKS

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => res.send('Hola desde mi app desplegada en AKS 🚀'));
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
